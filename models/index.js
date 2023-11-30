d8a2408335c94c535ee42f3b5226762e7d52e852
Glidingdiplomat <yankeefan1246@gmail.com>
2023-11-30 15:19:13 -0500

    created index.js
    importing and exporting user and thought models

---
+++ models/index.js
@@ -0,0 +1,5 @@
+// importing the user and thought models
+const User = require("./User");
+const Thought = require("./Thought");
+// exporting the user and thought models
+module.exports = { Thought, User };
