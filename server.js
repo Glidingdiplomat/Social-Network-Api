fcc24a6074c17fd1575fc7f90381011ed384e862
Glidingdiplomat <yankeefan1246@gmail.com>
2023-12-01 01:07:55 -0500

    initial commit
    server.js added user-routes within routes folder

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 const express = require("express");
+const db = require("./config/connections");
+const routes = require("./routes");
+// setting up environment variables
+const PORT = process.env.PORT || 3001;
+const app = express();
+// we are using middleware to parse incoming data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// this is to using routes to defined in routes.js
+app.use(routes);
+// connecting to the mongoDb database and to start the server
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
